6e2875d52d760f0eab8e0c2e9e4d057a6f19dbef
JERUKA9 <jeruka9@gmail.com>
2021-02-08 17:27:16 +0100

    Add files via upload

---
+++ Calc_no_gui.pas
@@ -0,0 +1,22 @@
+program Calc_no_gui;
+var
+
+  a,b,c,d,f,g,h:integer;
+   {In next revision  with array  functions  and procedures also sets and records also  const values}
+   {But first i need to learn them  so this program  is a silly test}
+
+begin
+   readln(a);
+   readln(b);
+   readln(c);
+   readln(d);
+   readln(f);
+   readln(g);
+   readln(h);
+   writeln(a*b*c*d*f*g*h);
+    writeln(a/b/c/d/f/g/h);
+     writeln(a-b-c-d-f-g-h);
+      writeln(a+b+c+d+f+g+h);
+       writeln(a div b div c div d div f div g div h);
+        writeln(a mod b mod c mod d mod f mod g mod h);
+end.
